ffb459797779be84e551528f5e8ddb5a79ec999d
Lilu-B <lilu.bahirova@gmail.com>
2024-11-27 14:18:50 +0000

    PASS tests for Task 6

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 const getApi = require("./controllers/api.controller");
 const getTopics = require('./controllers/topics.controller');
 const { getArticleById, getArticles } = require('./controllers/articles.controller');
+const getCommentsByArticleId = require('./controllers/comments.controller');
+
+app.use(express.json());
 
 app.get('/api', getApi);
 app.get('/api/topics', getTopics);
 app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 
 app.all('*', (req, res) => {
     res.status(404).send( {msg: 'Not Found'});
---
+++ models/comments.model.js
@@ -0,0 +1,19 @@
+const db = require('../db/connection');
+
+function fetchCommentsByArticleId(article_id) {
+  return db.query(
+      `SELECT comment_id, votes, created_at, author, body, article_id 
+       FROM comments 
+       WHERE article_id = $1
+       ORDER BY created_at DESC;`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+        if (rows.length === 0) {
+          return [];
+        }
+        return rows;
+    });
+};
+
+module.exports = fetchCommentsByArticleId;
