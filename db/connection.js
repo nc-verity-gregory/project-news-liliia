e6b8c1929661a1d4e1db7a96daeec5b0cff710c2
Lilu-B <lilu.bahirova@gmail.com>
2024-12-02 01:26:22 +0000

    Updated database connection and environment variables

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,35 @@
+// const { Pool } = require('pg');
+// const ENV = process.env.NODE_ENV || 'development';
+
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
+
+// if (!process.env.PGDATABASE) {
+//   throw new Error('PGDATABASE not set');
+// }
+// console.log('PGDATABASE:', process.env.PGDATABASE);
+
+// module.exports = new Pool();
+
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-console.log('PGDATABASE:', process.env.PGDATABASE);
-if (!process.env.PGDATABASE) {
+
+const database = process.env.NODE_ENV === 'test' 
+  ? process.env.PGDATABASE_TEST 
+  : process.env.PGDATABASE;
+
+if (!database) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+console.log('PGDATABASE:', database);
+
+module.exports = new Pool({
+  database: database
+});
+
