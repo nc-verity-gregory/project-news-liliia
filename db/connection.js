27537c6180e160b2aade7efdfe9500537b16aa76
verity <verity.gregory@northcoders.com>
2024-12-11 15:53:02 +0000

    set up

--- db/connection.js
+++ db/connection.js
@@ -1,34 +1,16 @@
-// const { Pool } = require('pg');
-// const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-// require('dotenv').config({
-//   path: `${__dirname}/../.env.${ENV}`,
-// });
-
-// if (!process.env.PGDATABASE) {
-//   throw new Error('PGDATABASE not set');
-// }
-// console.log('PGDATABASE:', process.env.PGDATABASE);
-
-// module.exports = new Pool();
-
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
-
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-  if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("PGDATABASE or DATABASE_URL not set");
-  }
+const config = {};
 
-  const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+console.log(`**Running in ${ENV} ENV**`);
 
-  if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.max = 2;
-  }
-
-
-module.exports = new Pool( config );
+module.exports = new Pool(config);
